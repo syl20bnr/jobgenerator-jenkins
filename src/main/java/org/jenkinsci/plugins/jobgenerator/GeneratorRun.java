ac18e9c57d3010d5f741da0bf86c4e141be42fa7
syl20bnr <sylvain.benner@gmail.com>
2012-12-09 23:32:38 -0500

    evaluate conditional entries for builders and publisher at generation time.

--- src/main/java/org/jenkinsci/plugins/jobgenerator/GeneratorRun.java
+++ src/main/java/org/jenkinsci/plugins/jobgenerator/GeneratorRun.java
@@ -24,24 +24,32 @@ THE SOFTWARE.
 
 package org.jenkinsci.plugins.jobgenerator;
 
+import groovy.util.Eval;
 import hudson.Util;
+import hudson.XmlFile;
 import hudson.model.Build;
 import hudson.model.BuildListener;
 import hudson.model.ParameterValue;
 import hudson.model.Result;
 import hudson.model.TopLevelItem;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Cause;
+import hudson.model.Descriptor;
 import hudson.model.ParametersAction;
 import hudson.model.Run;
 import hudson.plugins.parameterizedtrigger.AbstractBuildParameters;
 import hudson.plugins.parameterizedtrigger.BuildTrigger;
 import hudson.plugins.parameterizedtrigger.BuildTriggerConfig;
+import hudson.util.XStream2;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -57,6 +65,7 @@ import java.util.logging.Logger;
 import jenkins.model.Jenkins;
 
 import org.apache.tools.ant.filters.StringInputStream;
+import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.Node;
@@ -65,6 +74,9 @@ import org.dom4j.Visitor;
 import org.dom4j.VisitorSupport;
 import org.dom4j.io.SAXReader;
 
+import org.jenkins_ci.plugins.run_condition.RunCondition;
+import org.jenkins_ci.plugins.run_condition.RunCondition.RunConditionDescriptor;
+import org.jenkins_ci.plugins.run_condition.common.AlwaysPrebuildRunCondition;
 import org.jenkinsci.plugins.jobgenerator.actions.*;
 import org.jenkinsci.plugins.jobgenerator.parameters.*;
 import org.jenkinsci.plugins.jobgenerator.parameterizedtrigger.*;
@@ -136,6 +148,8 @@ public class GeneratorRun extends Build<JobGenerator, GeneratorRun> {
         }
 
         protected Result doRun(BuildListener listener) throws Exception {
+            // TODO syl20bnr: This function is a big mess. I plan to
+            // refactoring it when it comes to add some tests.
             if(!this.checkParameters(listener)){
                 return Result.FAILURE;
             }
@@ -208,10 +222,42 @@ public class GeneratorRun extends Build<JobGenerator, GeneratorRun> {
                                   "hudson.model.ParametersDefinitionProperty");
                 this.removeNodeIfNoChild(doc, "generatedJobName");
                 this.removeNodeIfNoChild(doc, "generatedDisplayJobName");
+                // Evaluate conditions and gather builders and publishers
+                Element vroot = (Element) doc.selectSingleNode("//builders");
+                if(vroot != null){
+                    v = new EvaluateAndGatherSteps(vroot,
+                                               (AbstractBuild<?, ?>)getBuild(),
+                                               listener);
+                    vroot.accept(v);
+                    // replace build steps by the gathered one
+                    for (Iterator i = vroot.elementIterator(); i.hasNext();) {
+                        Element e = (Element) i.next();
+                        e.detach();
+                    }
+                    for (Element e: ((EvaluateAndGatherSteps)v).elements){
+                        vroot.add(e);
+                    }
+                }
+                vroot = (Element) doc.selectSingleNode("//publishers");
+                if(vroot != null){
+                    v = new EvaluateAndGatherSteps(vroot,
+                                               (AbstractBuild<?, ?>)getBuild(),
+                                               listener);
+                    vroot.accept(v);
+                    // replace build steps by the gathered one
+                    for (Iterator i = vroot.elementIterator(); i.hasNext();) {
+                        Element e = (Element) i.next();
+                        e.detach();
+                    }
+                    for (Element e: ((EvaluateAndGatherSteps)v).elements){
+                        vroot.add(e);
+                    }
+                }
                 // Create/Update Job
                 doc.normalize();
                 InputStream is = new ByteArrayInputStream(
                                                 doc.asXML().getBytes("UTF-8"));
+//                System.out.println(doc.asXML());
                 boolean created =
                     Jenkins.getInstance().getItem(expName) == null;
                 if(created){
@@ -427,8 +473,8 @@ public class GeneratorRun extends Build<JobGenerator, GeneratorRun> {
     class GatherElementsToRemoveVisitor extends VisitorSupport {
         public List<Element> toRemove = new ArrayList<Element>();
 
-        public GatherElementsToRemoveVisitor(){
-        }
+        public GatherElementsToRemoveVisitor(){}
+
         @Override
         public void visit(Element node) {
             String n = node.getName();
@@ -441,16 +487,75 @@ public class GeneratorRun extends Build<JobGenerator, GeneratorRun> {
         }
     }
 
-//    class EvaluateConditionalEntries extends VisitorSupport {
-//        @Override
-//        public void visit(Element node) {
-//            String n = node.getName();
-//            if(n.contains("GeneratorKeyValueParameterDefinition") ||
-//               n.contains("GeneratorChoiceParameterDefinition") ||
-//               n.contains("GeneratorKeyValueBuildParameters") ||
-//               n.contains("GeneratorCurrentParameters")){
-//                this.toRemove.add(node);
-//            }
-//        }
-//    }
+    class EvaluateAndGatherSteps extends VisitorSupport {
+        private final Element root;
+        private final AbstractBuild<?, ?> build;
+        private final BuildListener listener;
+        private boolean include;
+        public List<Element> elements;
+        public EvaluateAndGatherSteps(
+                Element root,
+                AbstractBuild<?, ?> build,
+                BuildListener listener){
+            this.root = root;
+            this.build = build;
+            this.listener = listener;
+            this.elements = new ArrayList<Element>();
+            this.include = false;
+        }
+
+        @Override
+        public void visit(Attribute node) {
+            String n = node.getName();
+            String v = node.getValue();
+            Element p = node.getParent();
+            boolean condelement = p.getName().equals("condition") ||
+                                  p.getName().equals("runCondition");
+            if(condelement &&
+               n.equals("plugin") && v.contains("run-condition")){
+                try {
+                    // convert this chunk of xml config to a file
+                    InputStream is = new ByteArrayInputStream(
+                                                  p.asXML().getBytes("UTF-8"));
+                    XStream2 xs = new XStream2();
+                    RunCondition rc = (RunCondition) xs.fromXML(is);
+                    this.include = rc.runPerform(this.build, listener);
+                } catch (UnsupportedEncodingException e) {
+                } catch (FileNotFoundException e) {
+                } catch (IOException e) {
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        @Override
+        public void visit(Element node) {
+            Element p = node.getParent();
+            String n = node.getName();
+            boolean notconditional = (p != null && p == this.root &&
+                                      !n.contains("ConditionalBuilder") &&
+                                      !n.contains("FlexiblePublisher"));
+            if (notconditional){
+                // regular step
+                this.elements.add(node.createCopy());
+            }
+            else if (this.include && (n.equals("buildStep") ||
+                                      n.equals("publisher"))){
+                // single step or publisher
+                Element ne = node.createCopy();
+                ne.setName(ne.attributeValue("class"));
+                ne.attribute("class").detach();
+                this.elements.add(ne);
+                this.include = false;
+            }
+            else if (this.include && n.equals("conditionalbuilders")){
+                // multiple steps
+                for (Iterator i = node.elementIterator(); i.hasNext();) {
+                    Element e = (Element) i.next();
+                    this.elements.add(e.createCopy());
+                }
+                this.include = false;
+            }
+        }
+    }
 }
